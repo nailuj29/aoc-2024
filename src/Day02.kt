0419e357882eae6caaf62b96b34ce0caec2633cb
Nailuj29 <nailuj29gaming@gmail.com>
2024-12-02 00:16:55 -0500

    Clean up Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -12,50 +12,32 @@ fun main() {
         return -1
     }
 
+    fun safe(line: List<Int>): Boolean {
+        val sign = incOrDec(line[0], line[1])
+        if (sign == 0) {
+            return false
+        }
+        for (i in 0..<line.size - 1) {
+            if (incOrDec(line[i], line[i + 1]) != sign) {
+                return false
+            }
+
+            val diff = abs(line[i] - line[i + 1])
+            if (diff < 1 || diff > 3) {
+                return false
+            }
+        }
+        return true
+    }
+
     fun part1(input: List<String>): Int {
         val lines = input.map { l -> l.split(" ").map { it.toInt() } }
 
-        var count = 0
-
-        outer@ for (line in lines) {
-            val sign = incOrDec(line[0], line[1])
-            if (sign == 0) {
-                continue
-            }
-            for (i in 0..<line.size - 1) {
-                if (incOrDec(line[i], line[i + 1]) != sign) {
-                    continue@outer
-                }
-
-                val diff = abs(line[i] - line[i + 1])
-                if (diff < 1 || diff > 3) {
-                    continue@outer
-                }
-            }
-            count++
-        }
-
-        return count
+        return lines.count { safe(it) }
     }
 
     fun part2(input: List<String>): Int {
-        fun safe(line: List<Int>): Boolean {
-            val sign = incOrDec(line[0], line[1])
-            if (sign == 0) {
-                return false
-            }
-            for (i in 0..<line.size - 1) {
-                if (incOrDec(line[i], line[i + 1]) != sign) {
-                    return false
-                }
 
-                val diff = abs(line[i] - line[i + 1])
-                if (diff < 1 || diff > 3) {
-                    return false
-                }
-            }
-            return true
-        }
 
         val lines = input.map { l -> l.split(" ").map { it.toInt() } }
 
