534146a42291a5957eab502deff8701bd2dcf90a
Nailuj29 <nailuj29gaming@gmail.com>
2024-12-05 00:38:41 -0500

    MY SCRIPT DELETED MY CODE AND ALSO DOESN'T WORK

--- src/NewDay.kt
+++ src/NewDay.kt
@@ -1,11 +1,15 @@
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 
-const val DAY = 5
+const val DAY = 7
 
 fun main() {
-    val template = File("src/template.kt").readText()
+    var template = File("src/template.kt").readText()
 
-    template.replace("{day}", DAY.toString())
+    template = template.replace("{day}", DAY.toString())
+
+    if (Files.exists(Path.of("src/Day${DAY.toString().padStart(2, '0')}.kt"))) { return }
 
     File("src/Day${DAY.toString().padStart(2, '0')}.kt").writeText(template)
 }
