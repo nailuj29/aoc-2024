b69732dc666ab1d8c5758e56443a63d124073a29
Nailuj29 <nailuj29gaming@gmail.com>
2024-12-06 00:37:58 -0500

    Improve creation script

--- src/NewDay.kt
+++ src/NewDay.kt
@@ -2,14 +2,21 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 
-const val DAY = 6
+const val DAY = 7
 
 fun main() {
     var template = File("src/template.kt").readText()
 
     template = template.replace("{day}", DAY.toString())
 
-    if (Files.exists(Path.of("src/Day${DAY.toString().padStart(2, '0')}.kt"))) { return }
+    if (Files.exists(Path.of("src/Day${DAY.toString().padStart(2, '0')}.kt"))) {
+        println("Source code file for today already exists, exiting.")
+        return
+    }
 
     File("src/Day${DAY.toString().padStart(2, '0')}.kt").writeText(template)
+    File("src/day${DAY}.txt").writeText("")
+    File("src/day${DAY}_test.txt").writeText("")
+
+    println("Files created successfully")
 }
